98b2f7dad001b8d5a4b37f822701428b6bfdcc33
Shivaraj D Makarabbi <shivrajdm112@gmail.com>
2025-01-09 17:16:35 +0530

    Initial commit

---
+++ String/StringMethods.py
@@ -0,0 +1,10 @@
+s1 = "I am Shiva"
+s2 = "My age is 24"
+s3 = "We are learning string methods"
+s4 = "Python is dynamically typed language"
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+
---
+++ String/UserInput.py
@@ -0,0 +1,3 @@
+num1 = int(input('Enter a num1'))
+num2 = int(input('Enter a num2'))
+print(f'Addition of {num1} and {num2} is {num1+num2}')
