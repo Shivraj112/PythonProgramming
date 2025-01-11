fe1c23ba9f3b631feef560517d8d4b822189f92f
Shivaraj D Makarabbi <shivrajdm112@gmail.com>
2025-01-11 14:13:27 +0530

    Initial Commit

--- Basics/simplepgm.py
+++ Basics/simplepgm.py
@@ -1,2 +1,5 @@
 #Python code to print "Hello World"
-print("Hello, World!")
+# print("Hello, World!")
+
+my_variable = 10        #Python knows this is a number
+my_variable = "Hello"   #Now Python knows it's a word
