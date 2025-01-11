35a728371bf55207f7dd98408e1944225feb872d
Shivaraj D Makarabbi <shivrajdm112@gmail.com>
2025-01-11 11:58:40 +0530

    Inital commit

---
+++ Variable/Overview.py
@@ -0,0 +1,3 @@
+#1. Python is dynamically typed programming language
+#2. In python while declaring variable , we not need to mention data types
+#3. Data Types will be assigned by language based on value stored in the variable.
